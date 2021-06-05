56ce054cc6a2282733d71848e296a3232a065555
YulinarAyuMulyani <Yulinar220@gmail.com>
2021-06-06 06:25:14 +0800

    Project IDEA

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        String namaDepan = "Yulinar";
+        String namaTengah = "Ayu";
+        String namaBelakang = "Mulyani";
+
+        int usia = 21;
+        int targetTahunKuliah = 2;
+        double ipk = 3.81;
+        char nilaiAbjad = 'A';
+        boolean cantik = true;
+        System.out.println(namaDepan + namaTengah + namaBelakang);
+    }
+}
