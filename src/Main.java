674cfec074b339ce7b9d62e04e992c7fc7784546
YulinarAyuMulyani <Yulinar220@gmail.com>
2021-06-06 08:50:10 +0800

    Membuat String dari ArrayChar

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,15 @@ public class Main {
         String namaTengah = "Ayu";
         String namaBelakang = "Mulyani";
 
+        char[] uniskaChar = {'U','N','I','S','K','A'};
+        String uniskaString = new String(uniskaChar);
+
         int usia = 21;
         int targetTahunKuliah = 2;
         double ipk = 3.81;
         char nilaiAbjad = 'A';
         boolean cantik = true;
         System.out.println(namaDepan + namaTengah + namaBelakang);
+        System.out.println(uniskaString);
     }
 }
