7c9e1fe3775626e945b45307710a168792b16b86
YulinarAyuMulyani <Yulinar220@gmail.com>
2021-06-13 10:35:13 +0800

    Tambahkan setLocationRelativeTo(null), setTitle, dan setDefaultCloseOperation

--- src/modul3/GuiByText.java
+++ src/modul3/GuiByText.java
@@ -4,6 +4,9 @@ import javax.swing.*;
 public class GuiByText extends JFrame {
     GuiByText(){
         setSize(640,480);
+        setLocationRelativeTo(null);
+        setTitle("Gui By Text");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
     public static void main(String[] args){
         GuiByText guiByText = new GuiByText();
