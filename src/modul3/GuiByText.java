9f2aea499d6e5a5554960e010518c57719ea7b64
YulinarAyuMulyani <Yulinar220@gmail.com>
2021-06-13 10:45:03 +0800

    JButton

--- src/modul3/GuiByText.java
+++ src/modul3/GuiByText.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 public class GuiByText extends JFrame {
     JLabel label1 = new JLabel("Coba Input 1");
     JTextField textField1 = new JTextField();
+    JButton button1 = new JButton("Greet");
 
     GuiByText(){
         setSize(640,480);
@@ -15,9 +16,11 @@ public class GuiByText extends JFrame {
         getContentPane().setLayout(null);
         getContentPane().add(label1);
         getContentPane().add(textField1);
+        getContentPane().add(button1);
 
         label1.setBounds(16,32 ,100,25);
         textField1.setBounds(124,32,100,25);
+        button1.setBounds(232,32,80,25);
     }
     public static void main(String[] args){
         GuiByText guiByText = new GuiByText();
