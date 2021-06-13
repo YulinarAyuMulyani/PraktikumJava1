7920e467905893b2be368ad221419e34c268a29a
YulinarAyuMulyani <Yulinar220@gmail.com>
2021-06-13 09:57:48 +0800

    Menjadi sederhana

--- src/model/Mahasiswa.java
+++ src/model/Mahasiswa.java
@@ -1,6 +1,7 @@
 package model;
 
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -9,18 +10,28 @@ public class Mahasiswa {
     String npm;
     String nama;
     Date tanggalLahir;
-
-    public Mahasiswa(String npm, String yuli, String s) {
-    }
+    final String POLA_TANGGAL = "dd-MM-yyyy";
+    final SimpleDateFormat SDF = new SimpleDateFormat(POLA_TANGGAL);
 
     void tampilkanAtribut() {
-    String polaTanggal = "dd-MM-yyyy";
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
+    //String polaTanggal = "dd-MM-yyyy";
+      //  SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
 
         System.out.println("Nama \t\t\t : " + this.nama);
         System.out.println("NPM \t\t\t : " + this.npm);
-        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(this.tanggalLahir));
-}
+        System.out.println("Tanggal Lahir \t : " + SDF.format(this.tanggalLahir));
+    }
+
+    public Mahasiswa(String npm, String nama, String tanggalLahir) {
+        this.npm = npm ;
+        this.nama = nama;
+        try{
+            this.tanggalLahir = SDF.parse(tanggalLahir);
+        } catch (ParseException e) {
+            System.err.println("Kesalahan Pada Tanggal Lahir");
+        }
+    }
+
     void menyapa(){
     System.out.println("Hai nama aku : " + this.nama);
 }
